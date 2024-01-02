f6638e6f5ed6ab2cb206c3abd8f8f262e8fb62c0
hassan1032 <khanhassan1032@gmail.com>
2024-01-02 22:30:09 +0530

    worked on header

---
+++ src/component/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactNavbar } from "overlay-navbar";
+
+const Header = () =>{
+  return (
+    <div>
+      <ReactNavbar/>
+    </div>
+  )
+}
+
+export default Header
