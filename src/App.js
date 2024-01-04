24c1f8dd2be7ed22dfba7d1ac11162ea580c79f0
hassan1032 <khanhassan1032@gmail.com>
2024-01-04 23:44:16 +0530

    some new Desining

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./component/layout/Header.js";
 import { BrowserRouter as Router} from "react-router-dom";
 import WebFont from 'webfontloader';
 import React from "react";
+import Footer from "./component/layout/Footer/Footer.js"
 function App() {
   React.useEffect(()=>{
     WebFont.load({
@@ -16,6 +17,7 @@ function App() {
    <>
       <Router>
       <Header/>
+      <Footer/>
       </Router>
 
    </>
---
+++ src/component/layout/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import playStore from "../../../images/";
+// import appStore from "../../../images";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+       <div>heading</div>
+       <div>heading</div>
+       <div>heading</div>
+       <div>heading</div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
