b5652ebab1963c239f131c28d76b184dfd47c4dd
hassan1032 <khanhassan1032@gmail.com>
2024-01-15 21:49:08 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-
-import './App.css';
+import "./App.css";
 import Header from "./component/layout/Header.js";
-import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import WebFont from 'webfontloader';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import WebFont from "webfontloader";
 import React from "react";
-import Footer from "./component/layout/Footer/Footer.js"
-import Home from "./component/Home/Home.js"
-import ProductDetails from "./component/Product/ProductDetails.js"
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
+import ProductDetails from "./component/Product/ProductDetails.js";
 function App() {
-  React.useEffect(()=>{
+  React.useEffect(() => {
     WebFont.load({
-      google:{
-        families:["Roboto","Droid Sans","Chilanka"],
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
-  },[]);
+  }, []);
   return (
-   <>
-   
+    <>
       <Router>
-      <Header/>
-      
-      <Routes>
-       <Route extact path="/" Component={Home}/>
-       <Route exact path="/product/:id" 
-          Component={ProductDetails} />
-       
-       </Routes>
-       <Footer/>
-      </Router>
-     
+        <Header />
 
-   </>
+        <Routes>
+          <Route extact path="/" Component={Home} />
+          <Route exact path="/product/:id" Component={ProductDetails} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
